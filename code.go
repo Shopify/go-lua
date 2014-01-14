5d1ba296ca829f20978a7fcf68f7c0a96a4a9766
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2014-01-14 17:50:47 -0500

    Initial cut of code generator

--- code.go
+++ code.go
@@ -8,6 +8,11 @@ const (
 )
 
 const (
+	noJump     = -1
+	noRegister = maxArgA
+)
+
+const (
 	oprAdd = iota
 	oprSub
 	oprMul
@@ -27,21 +32,27 @@ const (
 )
 
 type function struct {
-	f        *prototype
-	h        *table
-	previous *function
-	p        *parser
-	pc       int
-	isVarArg bool
+	f                      *prototype
+	h                      *table
+	previous               *function
+	p                      *parser
+	pc, jumpPC, lastTarget int
+	isVarArg               bool
+	bytecode               []instruction
 	// TODO ...
 }
 
-func (f *function) assert(cond bool) {
-	f.p.l.assert(cond)
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
 }
 
+func (f *function) assert(cond bool) { f.p.l.assert(cond) }
+
 func (f *function) code(i instruction) int {
-	// f.dischargeJumpPC()
+	f.dischargeJumpPC()
 	f.f.code = append(f.f.code, i) // TODO check that we always only append
 	f.f.lineInfo = append(f.f.lineInfo, int32(f.p.lastLine))
 	f.pc++
@@ -54,6 +65,97 @@ func (f *function) codeABC(op opCode, a, b, c int) int {
 	f.assert(cMode(op) != opArgN || c == 0)
 	f.assert(a <= maxArgA && b <= maxArgB && c <= maxArgC)
 	return f.code(createABC(op, a, b, c))
+}
+
+func (f *function) fixJump(pc, dest int) {
+	f.assert(dest != noJump)
+	offset := dest - (pc + 1)
+	if abs(offset) > maxArgSBx {
+		f.p.syntaxError("control structure too long")
+	}
+	f.bytecode[pc].setSBx(offset)
+}
+
+func (f *function) label() int {
+	f.lastTarget = f.pc
+	return f.pc
+}
+
+func (f *function) jump(pc int) int {
+	if offset := f.bytecode[pc].sbx(); offset != noJump {
+		return pc + 1 + offset
+	}
+	return noJump
+}
+
+func (f *function) jumpControl(pc int) *instruction {
+	if pc >= 1 && testTMode(f.bytecode[pc-1].opCode()) {
+		return &f.bytecode[pc-1]
+	}
+	return &f.bytecode[pc]
+}
+
+func (f *function) patchTestRegister(node, register int) bool {
+	if i := f.jumpControl(node); i.opCode() != opTestSet {
+		return false
+	} else if register != noRegister && register != i.b() {
+		i.setA(register)
+	} else {
+		*i = createABC(opTest, i.b(), 0, i.c())
+	}
+	return true
+}
+
+func (f *function) patchListHelper(list, target, register, defaultTarget int) {
+	for list != noJump {
+		next := f.jump(list)
+		if f.patchTestRegister(list, register) {
+			f.fixJump(list, target)
+		} else {
+			f.fixJump(list, defaultTarget)
+		}
+		list = next
+	}
+}
+
+func (f *function) dischargeJumpPC() {
+	f.patchListHelper(f.jumpPC, f.pc, noRegister, f.pc)
+	f.jumpPC = noJump
+}
+
+func (f *function) patchList(list, target int) {
+	if target == f.pc {
+		f.patchToHere(list)
+	} else {
+		f.assert(target < f.pc)
+		f.patchListHelper(list, target, noRegister, target)
+	}
+}
+
+func (f *function) patchClose(list, level int) {
+	for level, next := level+1, 0; list != noJump; list = next {
+		next = f.jump(list)
+		f.assert(f.bytecode[list].opCode() == opJump && f.bytecode[list].a() == 0 || f.bytecode[list].a() >= level)
+		f.bytecode[list].setA(level)
+	}
+}
+
+func (f *function) patchToHere(list int) {
+	f.label()
+	f.concat(&f.jumpPC, list)
+}
+
+func (f *function) concat(l1 *int, l2 int) {
+	switch {
+	case l2 == noJump:
+	case *l1 == noJump:
+		*l1 = l2
+	default:
+		list := *l1
+		for next := f.jump(list); next != noJump; list, next = next, f.jump(next) {
+		}
+		f.fixJump(list, l2)
+	}
 }
 
 // func (f *function) expressionToRegisterOrConstant(e exprDesc) exprDesc {
--- instructions.go
+++ instructions.go
@@ -119,55 +119,35 @@ const (
 
 type instruction uint32
 
-func isConstant(x int) bool {
-	return 0 != x&bitRK
-}
-
-func constantIndex(r int) int {
-	return r & ^bitRK
-}
+func isConstant(x int) bool   { return 0 != x&bitRK }
+func constantIndex(r int) int { return r & ^bitRK }
 
 // creates a mask with 'n' 1 bits at position 'p'
-func mask1(n, p uint) instruction {
-	return ^(^instruction(0) << n) << p
-}
+func mask1(n, p uint) instruction { return ^(^instruction(0) << n) << p }
 
 // creates a mask with 'n' 0 bits at position 'p'
-func mask0(n, p uint) instruction {
-	return ^mask1(n, p)
+func mask0(n, p uint) instruction { return ^mask1(n, p) }
+
+func (i instruction) opCode() opCode         { return opCode(i >> posOp & mask1(sizeOp, 0)) }
+func (i instruction) arg(pos, size uint) int { return int(i >> pos & mask1(size, 0)) }
+
+func (i *instruction) setArg(pos, size uint, arg int) {
+	*i = *i&mask0(size, pos) | instruction(arg)<<pos&mask1(size, pos)
 }
 
-func (i instruction) opCode() opCode {
-	return opCode(i >> posOp & mask1(sizeOp, 0))
-}
+func (i instruction) a() int   { return i.arg(posA, sizeA) }
+func (i instruction) b() int   { return i.arg(posB, sizeB) }
+func (i instruction) c() int   { return i.arg(posC, sizeC) }
+func (i instruction) bx() int  { return i.arg(posBx, sizeBx) }
+func (i instruction) ax() int  { return i.arg(posAx, sizeAx) }
+func (i instruction) sbx() int { return i.bx() - maxArgSBx }
 
-func (i instruction) arg(pos, size uint) int {
-	return int(i >> pos & mask1(size, 0))
-}
-
-func (i instruction) a() int {
-	return i.arg(posA, sizeA)
-}
-
-func (i instruction) b() int {
-	return i.arg(posB, sizeB)
-}
-
-func (i instruction) c() int {
-	return i.arg(posC, sizeC)
-}
-
-func (i instruction) bx() int {
-	return i.arg(posBx, sizeBx)
-}
-
-func (i instruction) ax() int {
-	return i.arg(posAx, sizeAx)
-}
-
-func (i instruction) sbx() int {
-	return i.bx() - maxArgSBx
-}
+func (i instruction) setA(arg int)   { i.setArg(posA, sizeA, arg) }
+func (i instruction) setB(arg int)   { i.setArg(posB, sizeB, arg) }
+func (i instruction) setC(arg int)   { i.setArg(posC, sizeC, arg) }
+func (i instruction) setBx(arg int)  { i.setArg(posBx, sizeBx, arg) }
+func (i instruction) setAx(arg int)  { i.setArg(posAx, sizeAx, arg) }
+func (i instruction) setSBx(arg int) { i.setArg(posBx, sizeBx, arg+maxArgSBx) }
 
 func createABC(op opCode, a, b, c int) instruction {
 	return instruction(op)<<posOp |
@@ -176,19 +156,15 @@ func createABC(op opCode, a, b, c int) instruction {
 		instruction(c)<<posC
 }
 
-func createAbx(op opCode, a, bx int) instruction {
+func createABx(op opCode, a, bx int) instruction {
 	return instruction(op)<<posOp |
 		instruction(a)<<posA |
 		instruction(bx)<<posBx
 }
 
-func createAx(op opCode, a int) instruction {
-	return instruction(op)<<posOp | instruction(a)<<posAx
-}
+func createAx(op opCode, a int) instruction { return instruction(op)<<posOp | instruction(a)<<posAx }
 
-func opmode(t, a, b, c, m int) byte {
-	return byte(t<<7 | a<<6 | b<<4 | c<<2 | m)
-}
+func opmode(t, a, b, c, m int) byte { return byte(t<<7 | a<<6 | b<<4 | c<<2 | m) }
 
 const (
 	opArgN = iota // argument is not used
@@ -197,25 +173,11 @@ const (
 	opArgK        // argument is a constant or register/constant
 )
 
-func opMode(m opCode) int {
-	return int(opModes[m] & 3)
-}
-
-func bMode(m opCode) byte {
-	return (opModes[m] >> 4) & 3
-}
-
-func cMode(m opCode) byte {
-	return (opModes[m] >> 2) & 3
-}
-
-func testAMode(m opCode) bool {
-	return opModes[m]&(1<<6) != 0
-}
-
-func testTMode(m opCode) bool {
-	return opModes[m]&(1<<7) != 0
-}
+func opMode(m opCode) int     { return int(opModes[m] & 3) }
+func bMode(m opCode) byte     { return (opModes[m] >> 4) & 3 }
+func cMode(m opCode) byte     { return (opModes[m] >> 2) & 3 }
+func testAMode(m opCode) bool { return opModes[m]&(1<<6) != 0 }
+func testTMode(m opCode) bool { return opModes[m]&(1<<7) != 0 }
 
 var opModes []byte = []byte{
 	//     T  A    B       C     mode		    opcode
