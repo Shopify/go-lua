7bcc9d884977a508842d7e61e6570f7ec10b2cfb
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2014-02-20 15:08:58 -0500

    Coalesce some of the _test files

--- vm_test.go
+++ vm_test.go
@@ -1,6 +1,9 @@
 package lua
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestVm(t *testing.T) {
 	l := NewState()
@@ -26,4 +29,68 @@ func TestProtectedCall(t *testing.T) {
 	}, MaskCount, 1)
 	LoadString(l, "assert(not pcall(bit32.band, {}))")
 	Call(l, 0, 0)
+}
+
+func TestBit32(t *testing.T) {
+	l := NewState()
+	OpenLibraries(l)
+	LoadFile(l, "fixtures/bitwise.lua", "text")
+	Call(l, 0, 0)
+}
+
+func TestMath(t *testing.T) {
+	l := NewState()
+	OpenLibraries(l)
+	LoadFile(l, "fixtures/math.lua", "text")
+	Call(l, 0, 0)
+}
+
+func TestTableUnpack(t *testing.T) {
+	l := NewState()
+	OpenLibraries(l)
+	LoadString(l, "local x, y = table.unpack({-10,0}); assert(x == -10 and y == 0)")
+	Call(l, 0, 0)
+}
+
+func TestBase(t *testing.T) {
+	s := `
+    print("hello world\n")
+    assert(true)`
+	l := NewState()
+	BaseOpen(l)
+	LoadString(l, s)
+	Call(l, 0, 0)
+}
+
+func TestGoto(t *testing.T) {
+	l := NewState()
+	OpenLibraries(l)
+	LoadFile(l, "fixtures/goto.lua", "text")
+	Call(l, 0, 0)
+}
+
+func Example() {
+	type step struct {
+		name     string
+		function interface{}
+	}
+	steps := []step{}
+	l := NewState()
+	BaseOpen(l)
+	_ = NewMetaTable(l, "stepMetaTable")
+	SetFunctions(l, []RegistryFunction{{"__newindex", func(l *State) int {
+		k, v := CheckString(l, 2), ToValue(l, 3)
+		steps = append(steps, step{name: k, function: v})
+		return 0
+	}}}, 0)
+	PushUserData(l, steps)
+	PushValue(l, -1)
+	SetGlobal(l, "step")
+	SetMetaTableNamed(l, "stepMetaTable")
+	LoadString(l, `step.request_tracking_js = function ()
+	  get(config.domain..'/javascripts/shopify_stats.js')
+	end`)
+	Call(l, 0, 0)
+	fmt.Println(steps[0].name)
+	// Output: request_tracking_js
 }
