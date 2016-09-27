f192318fa34c75e64ec6a59e23586b2a02d7b834
Alex Mayfield <alexmax2742@gmail.com>
2016-09-26 22:10:09 -0400

    Improve unit test.
    Change the format string unit test to look for a specific string.

--- lua_test.go
+++ lua_test.go
@@ -1,8 +1,20 @@
 package lua
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
+// https://github.com/Shopify/go-lua/pull/63
 func TestPushFStringPointer(t *testing.T) {
 	l := NewState()
 	l.PushFString("%p %s", l, "test")
+
+	actual := CheckString(l, -1)
+	ok, err := regexp.MatchString("0x[0-9a-f]+ test", actual)
+	if !ok {
+		t.Error("regex did not match")
+	} else if err != nil {
+		t.Errorf("regex error: %s", err.Error())
+	}
 }
