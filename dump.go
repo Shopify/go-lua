4359a9b0e4714aa5c67c7eb88c219ba117160b59
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2017-03-05 17:09:53 -0500

    wip

--- dump.go
+++ dump.go
@@ -10,7 +10,75 @@ type dumpState struct {
   err error
 }
 
-func (d *dumpState) dumpFunction(p *prototype) {
+func (d *dumpState) writeInt(i int32) {
+  d.write(i)
+}
+
+func (d *dumpState) writeChar(i int32) {
+  x := rune(i)
+  d.write(x)
+}
+
+func (d *dumpState) writeCode(p *prototype) {
+  //d.writeInt(p.sizecode)
+  d.write(p.code)
+}
+
+func (d *dumpState) writeByte(b byte) {
+  d.write(b)
+}
+
+func (d *dumpState) writeBool(b bool) {
+  d.writeByte(b)
+}
+
+func (d *dumpState) writeNumber(f float64) {
+  d.write(f)
+}
+
+func (d *dumpState) writeString(s string) {
+
+}
+
+func (d *dumpState) writeConstants(p *prototype) {
+  for i := range p.constants {
+    var t byte
+    switch t, err = d.writeByte(t); {
+    case t == byte(TypeNil):
+      constants[i] = nil
+    case t == byte(TypeBoolean):
+      constants[i], err = state.readBool()
+    case t == byte(TypeNumber):
+      constants[i], err = state.readNumber()
+    case t == byte(TypeString):
+      constants[i], err = state.readString()
+    default:
+      err = errUnknownConstantType
+    }
+    if err != nil {
+      return
+    }
+  }
+}
+
+func (d *dumpState) writeUpvalues(p *prototype) {
+
+}
+
+func (d *dumpState) writeDebug(p *prototype) {
+
+}
+
+func (d *dumpState) dumpFunction(p *prototype) (err error) {
+  d.writeInt(p.lineDefined)
+  d.writeInt(p.lastLineDefined)
+  d.writeChar(p.parameterCount)
+  d.writeChar(p.isVarArg)
+  d.writeChar(p.maxStackSize)
+  d.writeCode(p)
+  d.writeConstants(p)
+  d.writeUpvalues(p)
+  d.writeDebug(p)
 
 }
 
--- dump_test.go
+++ dump_test.go
@@ -1,9 +1,4 @@
-// test dump header
-// create a dump state
-// out is a local byte buffer (ignore other fields)
-// call dump header
-// call in undump check header
-// reset byte buffer and create a load state taht wraps that with an order htat is endianness and call check header function on it
+
 // validates what you wrote you can read in successfuly
 
 package lua
