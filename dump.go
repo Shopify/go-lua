f3fc07a9e64b84babe9442d733052c9853588b54
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2017-05-08 15:01:55 -0400

    wip

--- dump.go
+++ dump.go
@@ -1,39 +1,44 @@
 package lua
 
 import (
-  "encoding/binary"
-  "io"
+	"encoding/binary"
+	"io"
 )
 
 type dumpState struct {
-  out io.Writer
-  err error
+	out   io.Writer
+	order binary.ByteOrder
+	err   error
+}
+
+func (d *dumpState) write(data interface{}) error {
+	return binary.Write(d.out, d.order, data)
 }
 
 func (d *dumpState) writeInt(i int32) {
-  d.write(i)
+	d.write(i)
 }
 
 func (d *dumpState) writeChar(i int32) {
-  x := rune(i)
-  d.write(x)
+	x := rune(i)
+	d.write(x)
 }
 
 func (d *dumpState) writeCode(p *prototype) {
-  //d.writeInt(p.sizecode)
-  d.write(p.code)
+	//d.writeInt(p.sizecode)
+	d.write(p.code)
 }
 
-func (d *dumpState) writeByte(b byte) {
-  d.write(b)
+func (d *dumpState) writeByte(b byte) error {
+	return d.write(b)
 }
 
 func (d *dumpState) writeBool(b bool) {
-  d.writeByte(b)
+	d.writeByte(b)
 }
 
 func (d *dumpState) writeNumber(f float64) {
-  d.write(f)
+	d.write(f)
 }
 
 func (d *dumpState) writeString(s string) {
@@ -41,24 +46,38 @@ func (d *dumpState) writeString(s string) {
 }
 
 func (d *dumpState) writeConstants(p *prototype) {
-  for i := range p.constants {
-    var t byte
-    switch t, err = d.writeByte(t); {
-    case t == byte(TypeNil):
-      constants[i] = nil
-    case t == byte(TypeBoolean):
-      constants[i], err = state.readBool()
-    case t == byte(TypeNumber):
-      constants[i], err = state.readNumber()
-    case t == byte(TypeString):
-      constants[i], err = state.readString()
-    default:
-      err = errUnknownConstantType
-    }
-    if err != nil {
-      return
-    }
-  }
+	for i := range p.constants {
+		var n = p.sizek
+		writeInt(n)
+
+		for i := 0; i < n; i++ {
+			var o = p.k[i]
+			writeChar(o)
+
+			switch o {
+			case LUA_TNIL:
+				break
+			case LUA_TBOOLEAN:
+				DumpChar(bvalue(o), D)
+				break
+			case LUA_TNUMBER:
+				DumpNumber(nvalue(o), D)
+				break
+			case LUA_TSTRING:
+				DumpString(rawtsvalue(o), D)
+				break
+			default:
+				lua_assert(0)
+			}
+		}
+
+		n = p.sizep
+		writeInt(n)
+
+		for i = 0; i < n; i++ {
+			dumpFunction(p)
+		}
+	}
 }
 
 func (d *dumpState) writeUpvalues(p *prototype) {
@@ -70,26 +89,26 @@ func (d *dumpState) writeDebug(p *prototype) {
 }
 
 func (d *dumpState) dumpFunction(p *prototype) (err error) {
-  d.writeInt(p.lineDefined)
-  d.writeInt(p.lastLineDefined)
-  d.writeChar(p.parameterCount)
-  d.writeChar(p.isVarArg)
-  d.writeChar(p.maxStackSize)
-  d.writeCode(p)
-  d.writeConstants(p)
-  d.writeUpvalues(p)
-  d.writeDebug(p)
+	d.writeInt(p.lineDefined)
+	d.writeInt(p.lastLineDefined)
+	d.writeChar(p.parameterCount)
+	d.writeChar(p.isVarArg)
+	d.writeChar(p.maxStackSize)
+	d.writeCode(p)
+	d.writeConstants(p)
+	d.writeUpvalues(p)
+	d.writeDebug(p)
 
 }
 
 func (d *dumpState) dumpHeader() {
-  d.err = binary.Write(d.out, endianness(), header)
+	d.err = binary.Write(d.out, endianness(), header)
 }
 
 func (l *State) dump(p *prototype, w io.Writer) error {
-  d := dumpState{out: w}
-  d.dumpHeader()
-  d.dumpFunction(p)
+	d := dumpState{out: w}
+	d.dumpHeader()
+	d.dumpFunction(p)
 
-  return d.err
+	return d.err
 }
