f90620bc7ddc32589a8664cf0cfa1ce4fe6034cd
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2017-05-19 15:03:47 -0400

    added all dump methods

--- dump_test.go
+++ dump_test.go
@@ -1,22 +1,24 @@
-
 // validates what you wrote you can read in successfuly
 
 package lua
 
 import (
-  "testing"
-  "bytes"
-  "encoding/binary"
+	"bytes"
+	"encoding/binary"
+	"testing"
 )
 
 func TestDump(t *testing.T) {
-  out := new(bytes.Buffer)
-  err := binary.Write(out, endianness(), "hi")
+	out := new(bytes.Buffer)
+	err := binary.Write(out, endianness(), "hi")
 
-  d := dumpState{out, err}
+	d := dumpState{out, endianness(), err}
 
-  d.dumpHeader()
+	d.dumpHeader()
 
-  l := loadState{out, endianness()}
-  l.checkHeader()
+	l := loadState{out, endianness()}
+	l.checkHeader()
 }
+
+//undump and then dump and check if the same
+//os.open and then readall so it is in a buffer to make comparison easier
