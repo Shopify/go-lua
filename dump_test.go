0e4d4018dd8e0614e029230b54b76bb2336fdde1
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2017-05-24 15:02:38 -0400

    wip dump tests

--- dump_test.go
+++ dump_test.go
@@ -4,20 +4,48 @@ package lua
 
 import (
 	"bytes"
-	"encoding/binary"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
 func TestDump(t *testing.T) {
+	_, err := exec.LookPath("luac")
+	if err != nil {
+		t.Skipf("testing dump requires luac: %s", err)
+	}
+	source := filepath.Join("lua-tests", "checktable.lua")
+	binary := filepath.Join("lua-tests", "checktable.bin")
+	if err := exec.Command("luac", "-o", binary, source).Run(); err != nil {
+		t.Fatalf("luac failed to compile %s: %s", source, err)
+	}
+	file, err := os.Open(binary)
+	if err != nil {
+		t.Fatal("couldn't open checktable.bin")
+	}
+
+	l := NewState()
+	closure, err := l.undump(file, "test")
+	if err != nil {
+		offset, _ := file.Seek(0, 1)
+		t.Error("unexpected error", err, "at file offset", offset)
+	}
+	if closure == nil {
+		t.Error("closure was nil")
+	}
+	p := closure.prototype
+	if p == nil {
+		t.Fatal("prototype was nil")
+	}
+
+	_, err = filepath.ReadFile("checktable.lua")
+
 	out := new(bytes.Buffer)
-	err := binary.Write(out, endianness(), "hi")
-
-	d := dumpState{out, endianness(), err}
-
-	d.dumpHeader()
-
-	l := loadState{out, endianness()}
-	l.checkHeader()
+	err = l.dump(p, out)
+	if err != nil {
+		t.Error("unexpected error", err, "with testing dump")
+	}
 }
 
 //undump and then dump and check if the same
