fdbf4f70e739a9410ed54007bb7c1b608aa1196a
Siddhesh Poyarekar <siddhesh@gotplt.org>
2019-11-13 12:43:37 +0530

    Fix format string failures in two tests
    go_test.go and dump.go failed due to errors in values provided in
    format strings.  Fix both failure cases.

--- dump.go
+++ dump.go
@@ -97,7 +97,7 @@ func (d *dumpState) writeString(s string) {
 	case 4:
 		d.write(uint32(size))
 	default:
-		panic(fmt.Sprintf("unsupported pointer size (%d)"))
+		panic(fmt.Sprintf("unsupported pointer size (%d)", header.PointerSize))
 	}
 	if size > 0 {
 		d.write(ba)
