a0375912c816791e368d509c202b34b70e82ab46
陈智 <chenz@feiyu.com>
2020-09-16 11:18:50 +0800

    table using sync.Pool

--- tables.go
+++ tables.go
@@ -2,21 +2,19 @@ package lua
 
 import (
 	"math"
+	"runtime"
+	"sync"
 )
 
-type table struct {
-	array         []value
-	hash          map[value]value
-	metaTable     *table
-	flags         byte
-	iterationKeys []value
+var (
+	tablePool sync.Pool
+)
+
+func init() {
+	tablePool = sync.Pool{New: func() interface{} { return newTableNoPool(0, 0) }}
 }
 
-func newTable() *table                     { return &table{hash: make(map[value]value)} }
-func (t *table) invalidateTagMethodCache() { t.flags = 0 }
-func (t *table) atString(k string) value   { return t.hash[k] }
-
-func newTableWithSize(arraySize, hashSize int) *table {
+func newTableNoPool(arraySize int, hashSize int) *table {
 	t := new(table)
 	if arraySize > 0 {
 		t.array = make([]value, arraySize)
@@ -26,8 +24,50 @@ func newTableWithSize(arraySize, hashSize int) *table {
 	} else {
 		t.hash = make(map[value]value)
 	}
+	runtime.SetFinalizer(t, finiTable)
 	return t
 }
+
+func finiTable(t *table) {
+	t.metaTable = nil
+	t.flags = 0
+	t.array = t.array[0:0]
+	t.iterationKeys = t.iterationKeys[0:0]
+	if len(t.hash) > 0 {
+		t.hash = nil
+	}
+	tablePool.Put(t)
+}
+
+type table struct {
+	array         []value
+	hash          map[value]value
+	metaTable     *table
+	flags         byte
+	iterationKeys []value
+}
+
+func newTable() *table {
+	return newTableWithSize(0, 0)
+}
+
+func newTableWithSize(arraySize int, hashSize int) *table {
+	if t, ok := tablePool.Get().(*table); ok {
+		if t.hash == nil {
+			if hashSize > 0 {
+				t.hash = make(map[value]value, hashSize)
+			} else {
+				t.hash = make(map[value]value)
+			}
+		}
+		return t
+	} else {
+		return newTableNoPool(arraySize, hashSize)
+	}
+}
+
+func (t *table) invalidateTagMethodCache() { t.flags = 0 }
+func (t *table) atString(k string) value   { return t.hash[k] }
 
 func (l *State) fastTagMethod(table *table, event tm) value {
 	if table == nil || table.flags&1<<event != 0 {
